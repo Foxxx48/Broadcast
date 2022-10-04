585dfc4338cb403adf54210ef2613c31a5ff4634
Foxxx48 <Foxxdim@mail.ru>
2022-10-04 14:10:01 +0300

    Add custom receiver

--- app/src/main/java/com/fox/broadcast/MainActivity.kt
+++ app/src/main/java/com/fox/broadcast/MainActivity.kt
@@ -3,19 +3,31 @@ package com.fox.broadcast
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private val receiver = MyReceiver()
+    var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            sendBroadcast(
+                Intent(MyReceiver.ACTION_CLICK).apply {
+                    putExtra(MyReceiver.EXTRA_KEY, ++count)
+
+                }
+            )
+        }
+
 
         val intentFilter = IntentFilter().apply {
             addAction(Intent.ACTION_BATTERY_LOW)
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
             addAction(Intent.ACTION_BATTERY_OKAY)
+            addAction(MyReceiver.ACTION_CLICK)
         }
         registerReceiver(receiver, intentFilter)
     }
--- app/src/main/java/com/fox/broadcast/MyReceiver.kt
+++ app/src/main/java/com/fox/broadcast/MyReceiver.kt
@@ -9,16 +9,27 @@ class MyReceiver : BroadcastReceiver(){
     override fun onReceive(context: Context, intent: Intent?) {
        when (intent?.action) {
            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
-               Toast.makeText(context, "Airplane mode changed", Toast.LENGTH_SHORT).show()
+               val turnOn = intent.getBooleanExtra("state", false)
+               Toast.makeText(context, "Airplane mode changed State: $turnOn", Toast.LENGTH_SHORT).show()
            }
            Intent.ACTION_BATTERY_LOW -> {
+
                Toast.makeText(context, "Low battery", Toast.LENGTH_SHORT).show()
            }
            Intent.ACTION_BATTERY_OKAY -> {
                Toast.makeText(context, "Battery Ok", Toast.LENGTH_SHORT).show()
            }
+           ACTION_CLICK -> {
+               val count = intent.getIntExtra(EXTRA_KEY, 100500)
+               Toast.makeText(context, "Button clicked $count times", Toast.LENGTH_SHORT).show()
+           }
        }
 
+    }
+
+    companion object {
+        const val ACTION_CLICK = "action_click"
+        const val EXTRA_KEY = "extra_key"
     }
 
 
